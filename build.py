6dfdd3f090d729034e468a581d59c185e8a42935
Huang-Ming Huang <huangh@objectcomputing.com>
2019-01-21 08:54:17 -0600

    Replace clang libstdc++ with libstdc++11

--- build.py
+++ build.py
@@ -1,6 +1,17 @@
 from cpt.packager import ConanMultiPackager
+import platform
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(pure_c=False)
+    if platform.system() == "Linux":
+        filtered_builds = []
+        for settings, options, env_vars, build_requires in builder.builds:
+            if settings["compiler"] == "clang" and settings["compiler.libcxx"] == "libstdc++":
+                settings_libstdcxx11 = settings.copy()
+                settings_libstdcxx11["compiler.libcxx"] = "libstdc++11"
+                filtered_builds.append([settings_libstdcxx11, options, env_vars, build_requires])
+            else:
+                filtered_builds.append([settings, options, env_vars, build_requires])
+        builder.builds = filtered_builds
     builder.run()
